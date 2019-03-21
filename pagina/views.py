0822873e7a2d84ef2ce77162fc2ae2c2ab03b8c9
David DC <dedc_011596@hotmail.com>
2019-03-21 17:04:56 -0600

    nuevas platillas y videos

--- pagina/views.py
+++ pagina/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def vLandingpage(request):
     return render(request, 'landingpage.html')
+
+def vprueba(request):
+    return render(request, 'prueba.html')
--- presentaciontobe/urls.py
+++ presentaciontobe/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from pagina.views import vLandingpage
+from pagina.views import vLandingpage, vprueba
 
 urlpatterns = [
-    path('', vLandingpage, name = 'landingpage')
+    path('', vLandingpage, name = 'landingpage'),
+    path('prueba', vprueba, name = 'p')
 ]
